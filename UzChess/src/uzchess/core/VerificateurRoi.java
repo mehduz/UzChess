518fd66ad3d22088b11ea8753f7f3bc43c2122e1
20130317 <20130317@FUJIYAMA.etudiants.campus.villejuif>
2013-12-12 12:15:17 +0100

    Implementation du VerificateurReine

---
+++ UzChess/src/uzchess/core/VerificateurRoi.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package uzchess.core;
+
+/**
+ *
+ * @author 20130317
+ */
+public class VerificateurRoi extends Deplacement{
+
+    @Override
+    public boolean verifierDeplacement(Case dep, Case arr) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
