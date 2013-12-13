43f317a1c03894d1fc34cd3a3f3aaf34acc56f0c
duran <duran@duran-Satellite-P300>
2013-12-13 11:19:29 +0100

    verificateurPion

--- UzChess/src/uzchess/core/MoteurDeJeu.java
+++ UzChess/src/uzchess/core/MoteurDeJeu.java
@@ -1,5 +1,8 @@
 package uzchess.core;
 
+import uzchess.core.model.Case;
+import uzchess.core.model.Echiquier;
+
 public class MoteurDeJeu {
 
     public void verifierCoup() {
@@ -7,16 +10,46 @@ public class MoteurDeJeu {
 
     public void detecterEchec() 
     {
-        
-        
+        //utiliser isMenace de Echiquier...
+        // trouver case du roi et demander isMeance...
+        Echiquier ech = Echiquier.getInstance();
+        if(ech.isMenace( ech.getCaseRoiB() ) ) {
+           System.out.println("le Roi Blanc est en echec!");
+        }
+        if(ech.isMenace( ech.getCaseRoiN() ) ) {
+           System.out.println("le Roi Noir est en echec!");
+        }
     }
     
     public void detecterMat() {
+        Echiquier ech = Echiquier.getInstance();
+        Case caseRN=ech.getCaseRoiN();
+        Case caseRB=ech.getCaseRoiB();
+        byte colRN=caseRN.getColonne();
+        byte ligRN=caseRN.getLigne();
+        byte cmpt=0;
+        Case caseV=new Case();
+        for(byte i=-1;i<2;i++){
+            for(byte j=-1;j<2;j++){
+                caseV.setColonne((byte) (colRN+i));
+                caseV.setLigne((byte) (ligRN+j));
+                if(ech.isMenace(caseV)){
+                    cmpt++;
+                    System.out.println("case menacée");
+                }
+                    
+            }
+        }
+        if(cmpt==9)
+        {
+            //roi peut pas bouger...
+        }
+        else //pas mat...
+        ;
     }
 
     public void detecterPat() {
     }
-    
-    
 
+    
 }
