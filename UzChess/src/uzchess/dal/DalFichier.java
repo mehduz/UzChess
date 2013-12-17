8db600e0e4a0c5f835817ef7ec5ff845211f8b53
user <user@master-PC>
2013-12-17 20:27:19 +0100

    Immplementation methode mat

---
+++ UzChess/src/uzchess/dal/DalFichier.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package uzchess.dal;
+
+import java.util.Collection;
+import uzchess.core.JeuEchecs;
+
+/**
+ *
+ * @author user
+ */
+public class DalFichier implements IDal{
+
+    @Override
+    public void charger() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void sauvegarder() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Collection<JeuEchecs> getListePartie() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ UzChess/src/uzchess/dal/IDal.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package uzchess.dal;
+
+import java.util.Collection;
+import uzchess.core.JeuEchecs;
+
+/**
+ *
+ * @author user
+ */
+public interface IDal {
+    
+    public void charger();
+    public void sauvegarder();
+    public Collection<JeuEchecs> getListePartie();
+    
+}
