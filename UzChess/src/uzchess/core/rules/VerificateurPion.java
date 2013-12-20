4423443493b99202d50015e52cc2d6b6988db91b
duran <duran@duran-Satellite-P300>
2013-12-20 10:23:07 +0100

    Revert "statut pion"
    This reverts commit 798453380d777a64defacee9485cb4079eb1e671.

--- UzChess/src/uzchess/core/rules/VerificateurPion.java
+++ UzChess/src/uzchess/core/rules/VerificateurPion.java
@@ -3,15 +3,13 @@ package uzchess.core.rules;
 import uzchess.constantes.Couleur;
 import uzchess.constantes.Direction;
 import uzchess.core.JeuEchecs;
-import uzchess.core.StatutPion;
 import uzchess.core.model.Case;
 
 public class VerificateurPion implements Deplacement {
 
 //penser  dep début, prise diago
     private JeuEchecs jeu;
-    private StatutPion st; 
-   
+    
     @Override
     public boolean verifierDeplacement(Case dep, Case arr, boolean noticeMove) {
 
@@ -29,18 +27,11 @@ public class VerificateurPion implements Deplacement {
         byte ligArr = arr.getLigne();
         byte dist = (byte) Math.abs(ligArr - ligDep);
         byte nbCoups = jeu.getCompteurCoups();
-        boolean hasmoved=st.getPions().get(dep.getPiece());
+        
         boolean condition1 = ( c == Couleur.BLANC && dir == Direction.N) || (c == Couleur.NOIR && dir == Direction.S);
         boolean condition2 =  dist == 1;
-        //se servir du fait qu'il ai bougé ou pas
+        boolean condition3 = (dist == 2 && (( nbCoups == 0  && c == Couleur.BLANC) || ( (nbCoups == 1) && (c == Couleur.NOIR))));
         
-        boolean condition3 = (dist == 2 && !hasmoved );
-        
-            if(condition1 && (condition2 || condition3)){ 
-                st.getPions().put(dep.getPiece(), true);
-                return true;
-            }
-             // verif dans statutPion.pions à l'indice "piece courante" si true..
-        return false;
+        return condition1 && (condition2 || condition3);
     }
 }
