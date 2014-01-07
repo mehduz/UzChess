72b2940eac6fabe9cd574061f5a86f9b4e3aaa3b
user <user@master-PC>
2014-01-08 00:02:52 +0100

    Debugging verificateur + Utilities terminés

--- UzChess/src/uzchess/core/domain/Echiquier.java
+++ UzChess/src/uzchess/core/domain/Echiquier.java
@@ -32,11 +32,7 @@ public class Echiquier implements Cloneable {
         this.st = st;
         this.sr = sr;
     }
-
-    public Case[][] getEchiquier() {
-        return cases;
-    }
-
+    
     public Case[][] getCases() {
         return cases;
     }
@@ -113,23 +109,23 @@ public class Echiquier implements Cloneable {
         }
         if (dep == caseRoiB) {
             sr.getRois().put(p, true);
-            byte decal = (byte)(arr.getColonne()- dep.getColonne());
-            if(decal == 2 && caseRoiB.getColonne() == 4){
-                deplacer(cases[7][7], cases[7][5]);
-            }
-            else if(decal == -2 && caseRoiB.getColonne() == 4 ){
-                deplacer(cases[7][0], cases[7][3]);
+            byte decal = (byte) (arr.getColonne() - dep.getColonne());
+            Piece pdep = dep.getPiece();
+            if ( decal == 2 ) {
+               deplacer(cases[7][7], cases[7][5]);
+            } else if (decal == -2) {
+               deplacer(cases[7][0], cases[7][3]);
             }
             caseRoiB = arr;
         }
         if (dep == caseRoiN) {
             sr.getRois().put(p, true);
-            byte decal = (byte)(arr.getColonne()- dep.getColonne());
-            if(decal == 2 && caseRoiN.getColonne() == 4){
-                deplacer(cases[0][7], cases[0][5]);
-            }
-            else if(decal == -2 && caseRoiN.getColonne() == 4){
-                deplacer(cases[0][0], cases[0][3]);
+            byte decal = (byte) (arr.getColonne() - dep.getColonne());
+            Piece pdep = dep.getPiece();
+            if (decal == 2 ) {
+                deplacer(cases[7][7], cases[7][5]);
+            } else if (decal == -2 ) {
+               deplacer(cases[7][0], cases[7][3]);
             }
             caseRoiN = arr;
         }
